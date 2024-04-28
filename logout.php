8b884c5b65608fa56cd1c09a7e7bfed732be472f
Issa Assi <30328675+issaassii@users.noreply.github.com>
2024-04-28 23:51:19 +0300

    Update logout.php
    Fixed issue where logout.php did not check if user is logged in or not.

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php 
     session_start();
+    if(!isset($_SESSION['username'])){
+        echo "You are not logged in.<br>";
+        echo "Redirecting you in 3 seconds";
+        header("refresh:3;url=login.php" );
+        exit();
+    }
     setcookie(session_name(), '', 100);
     session_unset();
     session_destroy();
